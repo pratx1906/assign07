ebe00048f9fc633ebabcbf284f5f94d5c163d9ae
pratx1906 <57968015+pratx1906@users.noreply.github.com>
2020-10-19 00:52:58 +0530

    Uploading all files

---
+++ assign07/src/assign07/GraphUtility.java
@@ -0,0 +1,121 @@
+package assign07;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Contains several methods for solving problems on generic, directed,
+ * unweighted, sparse graphs.
+ * 
+ * @author Erin Parker & ??
+ * @version October 14, 2020
+ */
+public class GraphUtility {
+
+	public static <Type> boolean areConnected(List<Type> sources, List<Type> destinations, Type srcData, Type dstData)
+			throws IllegalArgumentException {
+		// FILL IN + ADD METHOD COMMENT
+		return false;
+	}
+
+	public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData, Type dstData)
+			throws IllegalArgumentException {
+		// FILL IN + ADD METHOD COMMENT
+		return null;
+	}
+	
+	public static <Type> List<Type> sort(List<Type> sources, List<Type> destinations) throws IllegalArgumentException {
+		// FILL IN + ADD METHOD COMMENT
+		return null;
+	}
+
+	/**
+	 * Builds "sources" and "destinations" lists according to the edges
+	 * specified in the given DOT file (e.g., "a -> b"). Assumes that the vertex
+	 * data type is String.
+	 * 
+	 * Accepts many valid "digraph" DOT files (see examples posted on Canvas).
+	 * --accepts \\-style comments 
+	 * --accepts one edge per line or edges terminated with ; 
+	 * --does not accept attributes in [] (e.g., [label = "a label"])
+	 * 
+	 * @param filename - name of the DOT file
+	 * @param sources - empty ArrayList, when method returns it is a valid
+	 *        "sources" list that can be passed to the public methods in this
+	 *        class
+	 * @param destinations - empty ArrayList, when method returns it is a valid
+	 *        "destinations" list that can be passed to the public methods in
+	 *        this class
+	 */
+	public static void buildListsFromDot(String filename, ArrayList<String> sources, ArrayList<String> destinations) {
+
+		Scanner scan = null;
+		try {
+			scan = new Scanner(new File(filename));
+		}
+		catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+
+		scan.useDelimiter(";|\n");
+
+		// Determine if graph is directed (i.e., look for "digraph id {").
+		String line = "", edgeOp = "";
+		while(scan.hasNext()) {
+			line = scan.next();
+
+			// Skip //-style comments.
+			line = line.replaceFirst("//.*", "");
+
+			if(line.indexOf("digraph") >= 0) {
+				edgeOp = "->";
+				line = line.replaceFirst(".*\\{", "");
+				break;
+			}
+		}
+		if(edgeOp.equals("")) {
+			System.out.println("DOT graph must be directed (i.e., digraph).");
+			scan.close();
+			System.exit(0);
+
+		}
+
+		// Look for edge operator -> and determine the source and destination
+		// vertices for each edge.
+		while(scan.hasNext()) {
+			String[] substring = line.split(edgeOp);
+
+			for(int i = 0; i < substring.length - 1; i += 2) {
+				// remove " and trim whitespace from node string on the left
+				String vertex1 = substring[0].replace("\"", "").trim();
+				// if string is empty, try again
+				if(vertex1.equals(""))
+					continue;
+
+				// do the same for the node string on the right
+				String vertex2 = substring[1].replace("\"", "").trim();
+				if(vertex2.equals(""))
+					continue;
+
+				// indicate edge between vertex1 and vertex2
+				sources.add(vertex1);
+				destinations.add(vertex2);
+			}
+
+			// do until the "}" has been read
+			if(substring[substring.length - 1].indexOf("}") >= 0)
+				break;
+
+			line = scan.next();
+
+			// Skip //-style comments.
+			line = line.replaceFirst("//.*", "");
+		}
+
+		scan.close();
+	}
+}
